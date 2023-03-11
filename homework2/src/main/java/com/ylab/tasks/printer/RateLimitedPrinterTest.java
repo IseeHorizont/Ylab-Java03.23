85f55e94894d2ca96c6f638b358b33ef59cbb055
Evgeny <angst_eam@mail.ru>
2023-03-12 00:45:50 +0300

    task3:RateLimitedPrinter completed

---
+++ homework2/src/main/java/com/ylab/tasks/printer/RateLimitedPrinterTest.java
@@ -0,0 +1,11 @@
+package homework2.src.main.java.com.ylab.tasks.printer;
+
+public class RateLimitedPrinterTest {
+
+    public static void main(String[] args) {
+        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinter(1000);
+        for (int i = 0; i < 1_000_000_000; i++) {
+            rateLimitedPrinter.print(String.valueOf(i));
+        }
+    }
+}
