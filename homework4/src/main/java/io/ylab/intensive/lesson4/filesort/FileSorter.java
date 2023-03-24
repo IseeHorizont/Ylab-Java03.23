90fa773d0e78cef48e26bb54c7166950f2685c41
Evgeny <angst_eam@mail.ru>
2023-03-25 01:22:00 +0300

    init source project from https://github.com/gurinmd/ylab-lesson4-template

---
+++ homework4/src/main/java/io/ylab/intensive/lesson4/filesort/FileSorter.java
@@ -0,0 +1,7 @@
+package io.ylab.intensive.lesson4.filesort;
+
+import java.io.File;
+
+public interface FileSorter {
+  File sort(File data);
+}
---
+++ homework4/src/main/java/io/ylab/intensive/lesson4/movie/MovieLoader.java
@@ -0,0 +1,7 @@
+package io.ylab.intensive.lesson4.movie;
+
+import java.io.File;
+
+public interface MovieLoader {
+  void loadData(File file);
+}
